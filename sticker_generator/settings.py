72ba6300128a4acc4b9cc662fd2d42af27d055bb
q1bksuu <cpj1q@qq.com>
2024-09-24 13:31:55 +0800

    feat: allow any hosts

--- sticker_generator/settings.py
+++ sticker_generator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7v4$+1fdlx2s)f2g$&my4!fi4nv5zq!4s2$2=#8_==-jrav4f@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
